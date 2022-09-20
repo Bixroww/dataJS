a11f1a172eff713ba3daeb646a9c63a26ae9c3bb
Mehdi <112829615+Bixroww@users.noreply.github.com>
2022-09-20 10:01:53 +0200

    Add files via upload

---
+++ data js/index.js
@@ -0,0 +1,6 @@
+const array = ['Paris','Lyon', 'Bordeaux', 'Nigger']
+const array1 = ['Paris', 33, ['Bordeaux', 'Nigger'], true]
+
+for (i = 0; i < array.length; i++) {
+    console.log(typeof array1[i]);
+}
