7497414f532984995d91ef9947fd4ea2e69de1cc
Mehdi <112829615+Bixroww@users.noreply.github.com>
2022-09-21 20:44:28 +0200

    Update index.js

--- data js/index.js
+++ data js/index.js
@@ -16,7 +16,7 @@ const video = document.getElementById("video");
 let link = "";
 
 input.addEventListener("input", (e) => {
-  link = e.target.value;
+changeLink(e.target.value);
 
   if (link) {
     video.innerHTML = `<iframe width="600" height="300" src=${link} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
@@ -25,7 +25,10 @@ input.addEventListener("input", (e) => {
 
 const changeLink = (linkToChange) => {
     let embed = linkToChange.replace('watch?v=', "embed/");
-    link = embed
+    link = embed.split('&')[0];
+    console.log(link);
 }
 
 // https://youtu.be/QB1DTl7HFnc <iframe width="1903" height="772" src="https://www.youtube.com/embed/QB1DTl7HFnc" title="[Tuto Javascript #0] Débuter Javascript de zéro, apprendre les bases" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+//https://www.youtube.com/watch?v=QB1DTl7HFnc&t=625s
